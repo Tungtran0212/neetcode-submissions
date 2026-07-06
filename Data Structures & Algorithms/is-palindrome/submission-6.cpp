7dd4c2a31c519d563940a29d1c142d633dfc0750
Tungtran0212 <97713461+Tungtran0212@users.noreply.github.com>
2026-07-06 14:09:57 +0700

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.length();
+        string alphaString;
+        for (auto& x : s) {
+            x = tolower(x);
+        }
+        for(int i = 0; i<s.length(); i++)
+        {
+            if(isalnum(s[i]))
+                alphaString+=s[i];
+        }
+        int l = 0;
+        n = alphaString.length();
+        int r = n-1;
+        while(l != r && l < r)
+        {
+            if(alphaString[l] != alphaString[r])
+            {
+                cout<<alphaString[l]<< "  "<< l << "   " <<r<<"  "<<alphaString[r]<<endl;
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
